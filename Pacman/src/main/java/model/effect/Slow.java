f44584a5ff0d554236cac94fa0163c423c20def6
adele <adele.barbier1@etu.univ-lorraine.fr>
2020-10-25 16:39:26 +0100

    Documentatio manquantes sur les pièges (classes non complétées)

--- Pacman/src/main/java/model/effect/Slow.java
+++ Pacman/src/main/java/model/effect/Slow.java
@@ -3,6 +3,7 @@ package model.effect;
 /**
  * @author Adèle
  * 25/10/2020
+ * Ralentit le personnage dans ses déplacements
  **/
 public class Slow extends EffectTrap{
 
--- Pacman/src/main/java/model/effect/Stop.java
+++ Pacman/src/main/java/model/effect/Stop.java
@@ -3,6 +3,7 @@ package model.effect;
 /**
  * @author Adèle
  * 25/10/2020
+ * Arrête le personnage pendant un certain temps
  **/
 public class Stop extends EffectTrap{
 }
