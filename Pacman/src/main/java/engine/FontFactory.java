6f42af6ac3e9f66534118dacc6e1a4850b98952c
BadawyJr <a.badawy007@gmail.com>
2020-11-19 18:12:24 +0100

    Charger Font

---
+++ Pacman/src/main/java/engine/FontFactory.java
@@ -0,0 +1,29 @@
+package engine;
+
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+
+public class FontFactory {
+
+    private FontFactory(){}
+
+    private static FontFactory INSTANCE = new FontFactory();
+
+    public static FontFactory getInstance() {
+        return INSTANCE;
+    }
+
+    public Font LoadFont(String path,float size){
+        Font temp  = null;
+        try {
+            temp = Font.createFont(Font.TRUETYPE_FONT,new File("resources/"+path)).deriveFont(size);
+            GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+            ge.registerFont(Font.createFont(Font.TRUETYPE_FONT,new File("resources/"+path)));
+        } catch (IOException | FontFormatException e) {
+            System.out.println("Error loading font");
+        }
+
+        return temp;
+    }
+}
